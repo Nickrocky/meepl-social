67c563f9694c48d79f8edc52cf0238bcf9401e58
Nick <nick@tablebound.com>
2025-01-13 23:01:53 -0500

    Push SQL constants file for reference and testing

---
+++ meepl-social/Util/SQLCommands.cs
@@ -0,0 +1,30 @@
+namespace Meepl.Util;
+
+public class SQLCommands
+{
+    #region Platform Identifiers
+
+    public static string CREATE_META_PLATFORM_PROFILE_TABLE = ";";
+    public static string CREATE_STEAM_PLATFORM_PROFILE_TABLE = ";";
+    public static string CREATE_DISCORD_PLATFORM_PROFILE_TABLE = ";";
+    public static string CREATE_APPLE_PLATFORM_PROFILE_TABLE = ";";
+    public static string CREATE_EPIC_PLATFORM_PROFILE_TABLE = ";";
+    public static string CREATE_PASSKEYS_TABLE = ";";
+
+    #endregion
+    
+    public static string CREATE_TABLEBOUND_PROFILE_TABLE = ";";
+    public static string CREATE_PROFILE_BADGES_TABLE = ";";
+    public static string CREATE_SESSIONS_TABLE = ";";
+    public static string CREATE_FRIEND_REQUESTS_TABLE = ";";
+    public static string CREATE_EXPERIENCES_TABLE = ";";
+    public static string CREATE_BLOCKED_TABLE = ";";
+    public static string CREATE_FRIENDS_TABLE = ";";
+    public static string CREATE_CLANS_TABLE = ";";
+    public static string CREATE_BADGES_TABLE = ";";
+    public static string CREATE_EVENTS_TABLE = ";";
+    public static string CREATE_CLUBS_TABLE = ";";
+
+
+
+}
